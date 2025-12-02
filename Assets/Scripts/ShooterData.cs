7f6532a51bc0743b351dcc306e1f80d3de339fdf
Tygo Hendriks <157380506+tygohendriks@users.noreply.github.com>
2025-12-02 14:20:56 +0100

    update GunScript

--- Assets/Scripts/ShooterData.cs
+++ Assets/Scripts/ShooterData.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewShooterData", menuName = "Shooter/Shooter Data")]
 public class ShooterData : ScriptableObject
 {
     public float range = 100f;
-    public GameObject Gun;
 
     public GameObject fireEffect;
     public GameObject hitEffect;
 
     public float effectLifetime = 1f;
+
+    public GameObject Gun;
+
+    public Transform firePoint;   
 }
