2759eee51e8d546b971b096315b014539210dd73
Inflol13 <Jackvv12@outlook.com>
2025-12-02 09:22:06 +0100

    Oopsie with the render texture
    Replaced the render texture

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,9 +4,18 @@ public class PlayerHealth : MonoBehaviour
 {
     public int MaxHealth = 7;
     public int Health;
+
+    public Hearts Hearts;
     
     void Start()
     {
         Health = MaxHealth;
+        Hearts.UpdateHP(Health);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Health
+
     }
 }
