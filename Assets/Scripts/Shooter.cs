2493e1d2cb0db4fad53113a57c57424bc4cee2ef
Tygo Hendriks <157380506+tygohendriks@users.noreply.github.com>
2025-12-02 13:07:17 +0100

    Scripts Bijgewerkt

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,40 +3,48 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     public Camera fpsCam;
-    public Transform FirePoint;
-    public GameObject Fire;
-    public GameObject HitPoint;
+    public Transform firePoint;
 
+    public ShooterData shooterData;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Shooting();
-
         }
-    }        
-
-    public void Shooting() 
-    {
-     RaycastHit hit;
-
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 100f))
-        {
-            Debug.DrawRay(fpsCam.transform.position, fpsCam.transform.forward * hit.distance, Color.yellow);
-
-
-            GameObject a = Instantiate(Fire, FirePoint.position, Quaternion.identity);
-            GameObject b = Instantiate(HitPoint, hit.point, Quaternion.identity);
-
-            Destroy(a, 1f);
-            Destroy(b, 1f);
-
-        }
-
-
     }
 
+    public void Shooting()
+    {
+        RaycastHit hit;
 
+        if (Physics.Raycast(
+            fpsCam.transform.position,
+            fpsCam.transform.forward,
+            out hit,
+            shooterData.range))
+        {
+            Debug.DrawRay(
+                fpsCam.transform.position,
+                fpsCam.transform.forward * hit.distance,
+                Color.yellow
+            );
 
+            GameObject fire = Instantiate(
+                shooterData.fireEffect,
+                firePoint.position,
+                Quaternion.identity
+            );
+
+            GameObject hitFX = Instantiate(
+                shooterData.hitEffect,
+                hit.point,
+                Quaternion.identity
+            );
+
+            Destroy(fire, shooterData.effectLifetime);
+            Destroy(hitFX, shooterData.effectLifetime);
+        }
+    }
 }
