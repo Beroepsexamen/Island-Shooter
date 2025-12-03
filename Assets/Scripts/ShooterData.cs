2c76634c7b0c90880be07e7482dcc23e1a6955e2
Tygo Hendriks <157380506+tygohendriks@users.noreply.github.com>
2025-12-03 10:46:05 +0100

    Guns Added to list

--- Assets/Scripts/ShooterData.cs
+++ Assets/Scripts/ShooterData.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "NewShooterData", menuName = "Shooter/Shooter Data")]
 public class ShooterData : ScriptableObject
 {
     public float range = 100f;
