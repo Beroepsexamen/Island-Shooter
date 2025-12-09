3a5eb3297040f6d30f1fb6a8a4c65135a07fd10d
celestedegroot <celestemakesgames@gmail.com>
2025-12-09 12:56:47 +0100

    Working on player movement

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float SensX;
+    public float SensY;
+
+    public Transform PlayerObj;
+
+    float XRotation;
+    float YRotation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Lock cursor to center of screen
+        Cursor.visible = false; // Hide cursor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * SensX * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * SensY * Time.deltaTime;
+
+        YRotation += MouseX;
+        XRotation -= MouseY;
+
+        XRotation = Mathf.Clamp(XRotation, -90f, 90f); // Limit vertical look angle
+
+        transform.rotation = Quaternion.Euler(XRotation, YRotation, 0f); // Rotate camera based on mouse movement
+        PlayerObj.rotation = Quaternion.Euler(0f, YRotation, 0f); // Rotate player orientation based on mouse movement
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,172 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float MoveSpeed;
+
+    [HideInInspector] public float WalkSpeed;
+    [HideInInspector] public float SprintSpeed;
+
+    [Header("Ground Check")]
+    public float PlayerHeight;
+    public LayerMask Ground;
+    public float GroundDrag;
+    private bool Grounded;
+
+    [Header("Slope Handling")]
+    public float MaxSlopeAngle;
+    private RaycastHit SlopeHit;
+
+    public Transform PlayerObj;
+
+    float HorizontalInput;
+    float VerticalInput;
+
+    [Header("Jumping")]
+    public float JumpCooldown = 0.5f;
+    public float AirMultiplier = 0.4f;
+    public float JumpForce = 12f;
+    private bool ReadyToJump = true;
+
+    Vector3 MoveDirection;
+    Animator Animator;
+
+    private int XVelHash;
+    private int YVelHash;
+
+    Rigidbody RigidBody;
+
+    private void Start()
+    {
+        Animator = PlayerObj.GetComponent<Animator>();
+
+        XVelHash = Animator.StringToHash("XVelocity");
+        YVelHash = Animator.StringToHash("YVelocity");
+
+        RigidBody = GetComponent<Rigidbody>();
+        RigidBody.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        // Check if on ground
+        Grounded = Physics.Raycast(transform.position, Vector3.down, PlayerHeight + 0.1f, Ground);
+
+        if (Grounded)
+            RigidBody.linearDamping = GroundDrag;
+        else
+            RigidBody.linearDamping = 0;
+
+        MyInput();
+        SpeedControl();
+        AnimationControl();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
+        VerticalInput = Input.GetAxisRaw("Vertical");
+
+        // When to jump
+        if (Input.GetKey(KeyCode.Space) && ReadyToJump && Grounded)
+        {
+            ReadyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), JumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        Debug.Log(Grounded);
+
+        // Calculate movement direction
+        MoveDirection = PlayerObj.forward * VerticalInput + PlayerObj.right * HorizontalInput;
+
+        // On slope
+        if (OnSlope())
+        {
+            RigidBody.AddForce(GetSlopeMoveDirection() * MoveSpeed * 20f, ForceMode.Force);
+
+            if (RigidBody.linearVelocity.y <= 0)
+                RigidBody.AddForce(Vector3.down * 80f, ForceMode.Force);
+        }
+
+        // On ground
+        if (Grounded)
+            RigidBody.AddForce(MoveDirection.normalized * MoveSpeed * 10f, ForceMode.Force);
+
+        // In air
+        else if (!Grounded)
+            RigidBody.AddForce(MoveDirection.normalized * MoveSpeed * 10f * AirMultiplier, ForceMode.Force);
+
+        // Turn off gravity while on slope
+        RigidBody.useGravity = !OnSlope();
+    }
+
+    private void SpeedControl()
+    {
+        // Limiting speed on slopes
+        if (OnSlope())
+        {
+            if (RigidBody.linearVelocity.magnitude > MoveSpeed)
+                RigidBody.linearVelocity = RigidBody.linearVelocity.normalized * MoveSpeed;
+        }
+
+        // Limit velocity on ground
+        else
+        {
+            Vector3 FlatVel = new Vector3(RigidBody.linearVelocity.x, 0f, RigidBody.linearVelocity.z);
+
+            if (FlatVel.magnitude > MoveSpeed)
+            {
+                Vector3 LimitedVel = FlatVel.normalized * MoveSpeed;
+                RigidBody.linearVelocity = new Vector3(LimitedVel.x, RigidBody.linearVelocity.y, LimitedVel.z);
+            }
+        }
+    }
+
+    private void AnimationControl()
+    {
+        Vector3 FlatVel = new Vector3(RigidBody.linearVelocity.x, 0f, RigidBody.linearVelocity.z);
+
+        Animator.SetFloat(XVelHash, FlatVel.x);
+        Animator.SetFloat(YVelHash, FlatVel.z);
+    }
+
+    private void Jump()
+    {
+        RigidBody.linearVelocity = new Vector3(RigidBody.linearVelocity.x, 0f, RigidBody.linearVelocity.z);
+        RigidBody.AddForce(transform.up * JumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        ReadyToJump = true;
+    }
+
+    private bool OnSlope()
+    {
+        // Check if player is on a slope
+        if (Physics.Raycast(transform.position, Vector3.down, out SlopeHit, PlayerHeight * 0.5f + 0.2f))
+        {
+            float Angle = Vector3.Angle(Vector3.up, SlopeHit.normal);
+            return Angle < MaxSlopeAngle && Angle != 0;
+        }
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        // Get the direction to move on the slope
+        return Vector3.ProjectOnPlane(MoveDirection, SlopeHit.normal).normalized;
+    }
+}
