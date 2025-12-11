ca0f1cd443ac5859c57ced08154a2bb03c53a65f
Tygo Hendriks <157380506+tygohendriks@users.noreply.github.com>
2025-12-11 09:03:30 +0100

    Pickup Script change
    je kan nu E drukken om wat op te pakken

--- Assets/Scripts/PickUpTest.cs
+++ Assets/Scripts/PickUpTest.cs
@@ -4,14 +4,38 @@ public class GunPickup : MonoBehaviour
 {
     public ShooterData gunData;
 
+    private Shooter playerShooter;
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        // Alleen oppakken als speler dichtbij is en E drukt
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            playerShooter.AddGun(gunData);
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Shooter shooter = other.GetComponent<Shooter>();
 
         if (shooter != null)
         {
-            shooter.AddGun(gunData);
-            Destroy(gameObject);
+            playerShooter = shooter;
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Shooter shooter = other.GetComponent<Shooter>();
+
+        if (shooter != null)
+        {
+            playerShooter = null;
+            playerInRange = false;
         }
     }
 }
