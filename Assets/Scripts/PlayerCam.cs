1c6fad28288ed592eae5ee87870b446be3cfb6ff
celestedegroot <celestemakesgames@gmail.com>
2025-12-16 09:59:57 +0100

    Tuned player camera

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -26,7 +26,7 @@ public class PlayerCam : MonoBehaviour
         YRotation += MouseX;
         XRotation -= MouseY;
 
-        XRotation = Mathf.Clamp(XRotation, -90f, 90f); // Limit vertical look angle
+        XRotation = Mathf.Clamp(XRotation, -60f, 60f); // Limit vertical look angle
 
         transform.rotation = Quaternion.Euler(XRotation, YRotation, 0f); // Rotate camera based on mouse movement
         PlayerObj.rotation = Quaternion.Euler(0f, YRotation, 0f); // Rotate player orientation based on mouse movement
