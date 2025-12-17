3666b1638456b7e97050343a78f5e1402c2214a8
Tygo Hendriks <157380506+tygohendriks@users.noreply.github.com>
2025-12-17 10:49:01 +0100

    update Shooter

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -110,10 +110,6 @@ public class Shooter : MonoBehaviour
         if (Time.time < nextFireTime)
             return;
 
-        nextFireTime = Time.time + currentGunData.ShootDelay;
-        ammo.UseBullet(currentGunData);
-        UpdateAmmoUI();
-
         RaycastHit hit;
 
         if (Physics.Raycast(
@@ -122,6 +118,10 @@ public class Shooter : MonoBehaviour
             out hit,
             currentGunData.range))
         {
+            nextFireTime = Time.time + currentGunData.ShootDelay;
+            ammo.UseBullet(currentGunData);
+            UpdateAmmoUI();
+
             GameObject fire = Instantiate(
                 currentGunData.fireEffect,
                 currentFirePoint.position,
@@ -141,3 +141,4 @@ public class Shooter : MonoBehaviour
         }
     }
 }
+
