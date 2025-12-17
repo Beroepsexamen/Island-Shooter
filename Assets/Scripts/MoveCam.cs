f93e066dc88dec942e66ae3de891f586b6da84da
celestedegroot <celestemakesgames@gmail.com>
2025-12-17 09:49:07 +0100

    Fixed typo

--- Assets/Scripts/MoveCam.cs
+++ Assets/Scripts/MoveCam.cs
@@ -7,6 +7,6 @@ public class MoveCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = cameraPosition.position;AAW
+        transform.position = cameraPosition.position;
     }
 }
