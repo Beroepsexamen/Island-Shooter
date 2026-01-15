d32d553e9b5c5058417ea3deeaa10322d46aaaf7
Inflol13 <Jackvv12@outlook.com>
2026-01-15 09:51:40 +0100

    start aan achievement script

---
+++ Assets/Scripts/Achievements.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Achievements : MonoBehaviour
+{
+  public bool Secret1;
+  public bool Secret2;
+  public bool Secret3;
+  public bool Secret4;
+  public bool AllSecrets;
+  public bool EnemyKills5;
+  public bool EnemyKills10;
+  public bool EnemyKills15;
+  public bool RedDeGijzelaars;
+  public bool VindAlleGuns;
+};
+//public void UnlockAchievement(string achievementName)
+//{
+ //   switch (achievementName)
